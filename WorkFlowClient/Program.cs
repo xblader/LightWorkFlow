10a8daf3735d4b094985f4932c68f5b1c3e065ce
xblader <xblader@gmail.com>
2014-02-08 17:36:00 -0200

    version0.9

---
+++ WorkFlowClient/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkFlow;
+using WorkFlow.Business.Search;
+using WorkFlow.ConcreteCondition;
+using WorkFlow.Configuration;
+using WorkFlow.Context;
+using WorkFlow.DAO;
+using WorkFlow.Entities;
+using WorkFlow.Impl;
+
+namespace WorkFlowClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //put this line in a global.asax for web
+            WorkFlowConfiguration.Binder.SetRepository(typeof(DAOEmbeddedResource))
+                .Setup(x => x.TypeName, "WorkFlow.Json.movimentacao.json , WorkFlowMachine");
+
+            IWorkFlow work = WorkFlowManager.GetManager();
+            WorkFlowContext workc = work.GetContext()
+                .AddArea("BarraBotoesPDM")
+                .AddSourceState("EMRASCUNHO");
+
+            IList<string> depth = (IList<string>)work.Run(workc, SearchMode.Depth);
+
+            IList<string> breadth = (IList<string>)work.Run(workc, SearchMode.Breadth);
+
+            WorkFlowContext context = new WorkFlowContext {                 
+                 Area = "BarraBotoesPAD",
+                 Operation = "PAD_CONFIRMAR_CHEGADA_DESTINO",
+                 SourceState = "AGUARDAMOVIMENTAÇÃODESTINO"
+            }.SetCondition(typeof(NewMatch));
+
+            context["Finalidade"] = new List<string> { "2" };
+           
+            string state = work.GetNextStatus(context);
+        }
+    }
+
+    public class NewMatch : MatchCondition
+    {
+        public override bool CheckConditions(string condition, WorkFlowContext context)
+        {
+            if (condition == null) return true;
+
+            if (condition.StartsWith("!"))
+            {
+                Condition cond = GetNode().Conditions.Where(x => x.Name.Equals(condition.Substring(1))).FirstOrDefault();
+                return !CheckParameters(context, cond);
+            }
+            else
+            {
+                Condition cond = GetNode().Conditions.Where(x => x.Name.Equals(condition)).FirstOrDefault();
+                return CheckParameters(context, cond);
+            }
+        }
+    }
+}
